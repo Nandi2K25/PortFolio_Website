ca37f13ba85444b2f185c03386c28e7616fe0031
SUBAL KUMAR NANDI <subalnandi85@gmail.com>
2026-06-16 16:25:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const fades = document.querySelectorAll(".fade");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+fades.forEach(item => {
+    observer.observe(item);
+});
